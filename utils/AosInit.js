c8f861a303f30371a357f42887c16fe7065b3924
Errytagedesign <errytage@gmail.com>
2023-06-13 00:24:33 +0100

    Styling heirachy problem finally fixed

--- utils/AosInit.js
+++ utils/AosInit.js
@@ -1,5 +1,9 @@
 "use client";
 import { useEffect } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "aos/dist/aos.css";
 
 export default function AOSAnimation({ children }) {
   // AOS animation
