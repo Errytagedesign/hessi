82c71d119cd134a81a5a5029ff2958cd10d68de6
Errytagedesign <errytage@gmail.com>
2023-06-13 11:03:40 +0100

    navabr over padding fixed

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function About() {
-  return <div>Aboutiv </div>;
+  return (
+    <main
+      className={`${styles.about} d-flex flex-column justify-content-center `}
+    >
+      {/* Hero section */}
+      <section className={styles.aboutHero}>
+        <div className={styles.aboutHeroTitle}>
+          <h2 data-aos="fade-up-right" data-aos-duration="1500">
+            {" "}
+            Unlock Your Potential with our curated courses{" "}
+          </h2>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default About;
