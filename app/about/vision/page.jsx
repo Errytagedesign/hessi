ff6776f7dc5e336a0688f2c855d6bd27b7dfdaec
Errytagedesign <errytage@gmail.com>
2023-06-13 13:13:04 +0100

    vission page alignment fixed

--- app/about/vision/page.jsx
+++ app/about/vision/page.jsx
@@ -10,89 +10,94 @@ function Vision() {
     >
       <Herosection styles={styles} title=" Our Vision" />
 
-      <article className={` container text-center mt-5 ${styles.vision}`}>
-        <h3 className="mb-3 mt-4"> Vision</h3>
-        <p>
-          {" "}
-          To be known as the premier educational institute for advancing health
-          and healthcare.
-        </p>
-
-        <h3 className="mb-3 mt-4"> Core Values</h3>
-        <ul className="">
-          <li>
+      <article
+        className={` container d-flex flex-column flex-lg-row justify-content-between  mt-5 col-12 col-lg-6 ${styles.vision}`}
+      >
+        <div className="col-12 col-lg-6">
+          <h3 className="mb-3 mt-4"> Vision</h3>
+          <p>
             {" "}
-            <FaCheckCircle
-              className={styles.checkMark}
-              color="var(--secColor)"
-            />{" "}
-            Accountability
-          </li>
-          <li>
-            {" "}
-            <FaCheckCircle
-              color="var(--secColor)"
-              className={styles.checkMark}
-            />
-            Collaboration
-          </li>
-          <li>
-            {" "}
-            <FaCheckCircle
-              color="var(--secColor)"
-              className={styles.checkMark}
-            />
-            Competence
-          </li>
-          <li>
-            {" "}
-            <FaCheckCircle
-              color="var(--secColor)"
-              className={styles.checkMark}
-            />
-            Equity
-          </li>
-          <li>
-            {" "}
-            <FaCheckCircle
-              color="var(--secColor)"
-              className={styles.checkMark}
-            />
-            Excellence
-          </li>
-          <li>
-            {" "}
-            <FaCheckCircle
-              color="var(--secColor)"
-              className={styles.checkMark}
-            />
-            Inclusion
-          </li>
-          <li>
-            {" "}
-            <FaCheckCircle
-              color="var(--secColor)"
-              className={styles.checkMark}
-            />
-            Integrity
-          </li>
-          <li>
-            {" "}
-            <FaCheckCircle
-              color="var(--secColor)"
-              className={styles.checkMark}
-            />
-            Reliability
-          </li>
-          <li>
-            {" "}
-            <FaCheckCircle
-              color="var(--secColor)"
-              className={styles.checkMark}
-            />
-            Respect{" "}
-          </li>
-        </ul>
+            To be known as the premier educational institute for advancing
+            health and healthcare.
+          </p>
+        </div>
+        <div className="col-12 col-lg-6">
+          <h3 className="mb-3 mt-4"> Core Values</h3>
+          <ul className="">
+            <li>
+              {" "}
+              <FaCheckCircle
+                className={styles.checkMark}
+                color="var(--secColor)"
+              />{" "}
+              Accountability
+            </li>
+            <li>
+              {" "}
+              <FaCheckCircle
+                color="var(--secColor)"
+                className={styles.checkMark}
+              />
+              Collaboration
+            </li>
+            <li>
+              {" "}
+              <FaCheckCircle
+                color="var(--secColor)"
+                className={styles.checkMark}
+              />
+              Competence
+            </li>
+            <li>
+              {" "}
+              <FaCheckCircle
+                color="var(--secColor)"
+                className={styles.checkMark}
+              />
+              Equity
+            </li>
+            <li>
+              {" "}
+              <FaCheckCircle
+                color="var(--secColor)"
+                className={styles.checkMark}
+              />
+              Excellence
+            </li>
+            <li>
+              {" "}
+              <FaCheckCircle
+                color="var(--secColor)"
+                className={styles.checkMark}
+              />
+              Inclusion
+            </li>
+            <li>
+              {" "}
+              <FaCheckCircle
+                color="var(--secColor)"
+                className={styles.checkMark}
+              />
+              Integrity
+            </li>
+            <li>
+              {" "}
+              <FaCheckCircle
+                color="var(--secColor)"
+                className={styles.checkMark}
+              />
+              Reliability
+            </li>
+            <li>
+              {" "}
+              <FaCheckCircle
+                color="var(--secColor)"
+                className={styles.checkMark}
+              />
+              Respect{" "}
+            </li>
+          </ul>
+        </div>
       </article>
     </main>
   );
