90dc1d78600385e4fd2b6d0b5936f4e607f36a3e
Errytagedesign <errytage@gmail.com>
2023-06-19 18:35:31 +0100

    Added hessi logo to bottom

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import styles from "./page.module.scss";
 import Image from "next/image";
 import construction from "../public/assets/images/construction.webp";
+import logo from "../public/assets/images/hessi-logo.png";
 
 export default function Home() {
   return (
@@ -15,6 +16,10 @@ export default function Home() {
         We are currently under construction to bring you the best training and
         information possible, so stay tuned for updates.{" "}
       </h3>
+
+      <div className="col-7 col-md-2 mt-5 mb-5">
+        <Image src={logo} alt="" />
+      </div>
     </main>
   );
 }
